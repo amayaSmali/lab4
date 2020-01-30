c7defb728021a9f91bd253f9a5b5962a919e413e
Amaya Mali <asmali@ucsd.edu>
2020-01-30 11:59:48 -0800

    commmit

---
+++ routes/project.js
@@ -0,0 +1,11 @@
+exports.view = function(req, res){
+    res.render('index',{
+        'name': 'Waiting in Line',
+        'image' : 'lorempixel.people.1.jpeg',
+        'id' : 'project1'
+    });
+  };
+
+exports.viewProject = function(req, res){
+    res.render('Project');
+    }
